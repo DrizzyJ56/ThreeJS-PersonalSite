58ceef23e183a977fcf52552977a5b13e6e381fe
DrizzyJ56 <blevins.j921@gmail.com>
2023-04-17 15:37:45 -0400

    forcing amplify reload

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
       variants={fadeIn("","",0.1,1)}
       className='mt-4 text-secondary text-[17px] max-w-3xl leading-30[px]'
       >
-        I'm a skilled software developer with experience in.....
+        I'm a skilled software developer with experience in....
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
